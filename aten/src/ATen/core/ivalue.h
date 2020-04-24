c0ff1e56e39dbc9415a7da763d0f80edaf4e77eb
Jerry Zhang <jerryzh168@gmail.com>
2020-04-24 08:54:02 -0700

    [jit] Deepcopy for `script::Module`
    Summary:
    Previously we have `clone` and `clone_instance`, where `clone` will clone both type
    and value, and `clone_instance` only clone the value, both of them are shallow copies.
    We need to re-evaluate whether we should expose them as a user facing API.
    I think we should hide `clone`, but `clone_instance` might be useful as well, especially
    when we are copying a model with very large weights, people might just want to do shallow copy.
    
    This PR adds a `deepcopy` that might be useful as a user API, which deep copies the values, including
    Tensor, but we didn't deepcopy `Blob`, `Capsule`, `Future` or `PyObject`.
    For more discussions please see the following issue.
    
    fixes: https://github.com/pytorch/pytorch/issues/32519
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: b33f038679591649e4b69dc3cbcfd5ee27a870c9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/32684

--- aten/src/ATen/core/ivalue.h
+++ aten/src/ATen/core/ivalue.h
@@ -620,6 +620,7 @@ struct CAFFE2_API IValue final {
   };
 
   using HashAliasedIValues = std::unordered_set<IValue, HashAliasedIValue, CompAliasedIValues>;
+  using HashAliasedIValueMap = std::unordered_map<IValue, IValue, HashAliasedIValue, CompAliasedIValues>;
 
   // Chechs if this and rhs has a subvalues in common.
   // [t1,t2] and [t2, t3] returns true.
@@ -627,6 +628,10 @@ struct CAFFE2_API IValue final {
 
   // Inserts all subvalues of this in subValues.
   void getSubValues(HashAliasedIValues& subValues) const;
+
+  IValue deepcopy() const;
+  IValue deepcopy(
+      HashAliasedIValueMap& memo) const;
 
  private:
   static bool ptrEqual(const IValue& lhs, const IValue& rhs);
