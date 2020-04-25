be7bcd702bad8dbfd1c5d710521319e59a121d70
Shihao Xu <xsh6528@gmail.com>
2020-04-24 17:11:15 -0700

    [distribtued.nn] Implement TorchScript-compatible RemoteModule API
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37139
    
    See design doc in https://github.com/pytorch/pytorch/issues/37136
    ghstack-source-id: 102891900
    
    Differential Revision: [D20499658](https://our.internmc.facebook.com/intern/diff/D20499658/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D20499658/)!

---
+++ torch/distributed/nn/api/remote_module.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+import uuid
+from typing import Any, Callable, Dict, Tuple
+
+import torch
+import torch.distributed.rpc as rpc
+from torch.distributed.nn.jit import instantiator
+
+
+def _gen_global_unique_name():
+    return f"{uuid.uuid4().hex}"
+
+
+def _instantiate_template(global_unique_name, module_interface_cls, is_scriptable):
+    # Generate the template instance name.
+    generated_module_name = f"_remote_module_{global_unique_name}"
+
+    # Instantiate _RemoteModule class template on the local side.
+    generated_module = instantiator.instantiate_remote_module_template(
+        generated_module_name, module_interface_cls, is_scriptable
+    )
+
+    remote_module_cls = generated_module._RemoteModule
+
+    return remote_module_cls
+
+
+def _script_module_creator_wrapper(module_creator, module_interface_cls, args, kwargs):
+    module = module_creator(*args, **kwargs)
+    script_module = torch.jit.script(module)
+    return rpc.RRef(script_module, module_interface_cls)
+
+
+def RemoteModule(
+    to: str,
+    module_creator: Callable,
+    args: Tuple = None,
+    kwargs: Dict[str, Any] = None,
+    global_unique_name: str = None,
+    module_interface_cls=None,
+):
+    """
+        The returned RemoteModule instance will have auto-generated
+        forward_async / forward methods, check the implementation in
+        remote_module.py.template,
+        the arguments they take are the same as the forward method
+        of the module created by the module_creator.
+
+    Args:
+        to: str.
+        module_creator: Callable. The return type of the callable
+            must be a class decorated with @torch.jit.interface.
+    """
+    # Sanity checks.
+    assert rpc._is_current_rpc_agent_set(), "RemoteModule only works in RPC."
+
+    # Default arguments preperation.
+    args = args if args is not None else ()
+    kwargs = kwargs if kwargs is not None else {}
+    global_unique_name = (  # Assign a global name for the module to be created.
+        global_unique_name
+        if global_unique_name is not None
+        else _gen_global_unique_name()
+    )
+
+    # Infer module_interface type.
+    module_interface_cls = (
+        module_interface_cls
+        if module_interface_cls is not None
+        else instantiator.get_return_type_from_callable(module_creator)
+    )
+    is_scriptable = getattr(module_interface_cls, "__torch_script_interface__", False)
+
+    # Instantiate template on remote side.
+    fut = rpc.rpc_async(
+        to,
+        _instantiate_template,
+        (global_unique_name, module_interface_cls, is_scriptable),
+    )
+
+    # Instantiate template on local side.
+    remote_module_cls = _instantiate_template(
+        global_unique_name, module_interface_cls, is_scriptable
+    )
+
+    # Create the module on the remote side.
+    if is_scriptable:
+        module_rref = rpc.rpc_sync(
+            to,
+            _script_module_creator_wrapper,
+            (module_creator, module_interface_cls, args, kwargs),
+        )
+    else:
+        module_rref = rpc.remote(to, module_creator, args, kwargs)
+
+    # Create remote_module_cls instance on local side.
+    remote_module = remote_module_cls(module_rref, is_scriptable)
+
+    fut.wait()
+    return remote_module
---
+++ torch/distributed/nn/jit/templates/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+
+dir_path = os.path.dirname(__file__)
