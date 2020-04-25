1c307fc434592eb4f1561231b088cbb2f4b78b96
Michael Suo <suo@fb.com>
2020-04-24 17:54:20 -0700

    add fmt
    fmt is a formatting library for C++. It has several properties that make it nice
    for inclusion in PyTorch:
    - Widely used
    - Basically copies how Python does it
    - Support for all the compilers and platforms we care about
    - Standards track (C++20)
    - Small code size
    - Header only
    - The author works at FB (hi @vitaut).
    
    This PR includes it as a submodule and sets up the build. It also
    provides some example usage, including a `TORCH_CHECK_FMT` macro in c10
    that should test all the builds we care about.
    
    ghstack-source-id: b1af8ce3fab8d5af217ce3e69d93c7f2a91b97ac
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37241

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -596,7 +596,7 @@ if(USE_FBGEMM)
   caffe2_update_option(USE_FBGEMM ON)
 else()
   caffe2_update_option(USE_FBGEMM OFF)
-  message(WARNING 
+  message(WARNING
     "Turning USE_FAKELOWP off as it depends on USE_FBGEMM.")
   caffe2_update_option(USE_FAKELOWP OFF)
 endif()
@@ -1552,3 +1552,7 @@ endif()
 #
 # End ATen checks
 #
+
+set(FMT_INSTALL ON CACHE BOOL " " FORCE)
+add_subdirectory(${CMAKE_SOURCE_DIR}/third_party/fmt)
+list(APPEND Caffe2_DEPENDENCY_LIBS fmt::fmt)
